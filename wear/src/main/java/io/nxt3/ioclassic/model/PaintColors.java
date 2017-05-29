aa3d280de302ad079ff4dbb79eabe8c4ee9d7d25
nxt3 <nategantt@gmail.com>
2017-05-29 16:36:45 -0400

    Added PaintColor keys for each component

--- wear/src/main/java/io/nxt3/ioclassic/model/PaintColors.java
+++ wear/src/main/java/io/nxt3/ioclassic/model/PaintColors.java
@@ -3,4 +3,14 @@ package io.nxt3.ioclassic.model;
 
 public enum PaintColors {
 
+    /**
+     * Colors for each of the different components
+     * This is the KEY we'll be using for storing in Settings
+     */
+    HOUR_HAND_COLOR,
+    MINUTE_HAND_COLOR,
+    SECOND_HAND_COLOR,
+    BACKGROUND_COLOR,
+    CIRCLE_AND_TICKS_COLOR,
+    OUTER_CIRCLE_COLOR
 }
