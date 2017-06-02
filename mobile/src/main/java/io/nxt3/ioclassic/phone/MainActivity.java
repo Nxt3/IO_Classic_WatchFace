d78cd491481b018e5598c0313933735fb8f5feb4
nxt3 <nategantt@gmail.com>
2017-06-02 11:02:43 -0400

    Added shell mobile phone module so that users can find the watch face from the Play Store on their phone -WIP adding a tutorial to install the watch face from the phone app

---
+++ mobile/src/main/java/io/nxt3/ioclassic/phone/MainActivity.java
@@ -0,0 +1,12 @@
+package io.nxt3.ioclassic.phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
